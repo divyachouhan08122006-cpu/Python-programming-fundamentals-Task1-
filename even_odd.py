8ed4ecd3c666cfd5df440cf69b0e66d4a69f45a8
divyachouhan08122006-cpu <divyachouhan08122006@gmail.com>
2026-06-21 15:17:20 +0530

    Add files via upload
    task 1 python programs

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+#ques 2 print even or odd
+num=int(input("enter a number:"))
+if num%2==0:
+    print("given number is even.")
+else:
+    print("given number is odd.")
+
+
---
+++ factorial.py
@@ -0,0 +1,4 @@
+#ques3 factorial 
+from math import factorial
+a=int(input("enter a number:"))
+print(factorial(a))
